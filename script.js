2b2a306648ed3a061960f9c06004dea0d945e3a9
4FunkE <4FunkEcodes@gmail.com>
2023-06-20 21:04:51 -0400

    cleaned code

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const fs = require("fs")//to read and write files
 const inquirer = require("inquirer")//the inquiry files from JSON
 const generateMarkdown = require("./utils/generateMarkdown")//download markdown file
 
-// TODO: Create an array of questions for user input
-const questions = [
-    "What is the name of your project repo?",
-    "Please provide a short description explaining the what, why, and how of your project.",
-    "What are the steps required to install your project?",
-    "Please provide instructions and examples of the applications use.",
-    "List your collaborators.",
-    "Choose a liscense:",
-    "Any badges? If none type n/a.",
-    "Does you project have any features? If none type n/a.",
-    "Provide guidelines for how other developers can contribute. If none type n/a.",
-    "Enter test instructions:",
-    "Enter your GitHub username:",
-    "Enter your email address:"
-];
-
-//what will prompt the user for the information to generate README
+//array of questions for user input to generate README
 inquirer
   .prompt([
     {
@@ -84,14 +68,16 @@ inquirer
         name: 'email',
     },
   ])
+//to move answers to generate
   .then((answers) => {
     const readmeContent = generateMarkdown(answers);
-// TODO: Create a function to write README file
+//write README file
     fs.writeFile("README.md", readmeContent, (err) => {
         if (err) throw err;
-        console.log("You successfully created a REAADME!")
+        console.log("You successfully created a REAADME! DON'T FORGET, add a screenshot and the live URL to readme if applicable.")
     });
 })
+//error
 .catch((error) => {
     console.error(error);
 });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,6 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+//Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  # ${data.title}
+  return `# ${data.title}
 
   ## Description
   ${data.description}
@@ -31,9 +18,11 @@ function generateMarkdown(data) {
 
   ## Installation
   ${data.installation}
+  - Deployed Site --> 
 
   ## Usage
   ${data.usage}
+  ![alt text](assets/images/screenshot.png)
 
   ## Credits
   ${data.credits}
@@ -58,4 +47,5 @@ function generateMarkdown(data) {
 `;
 }
 
+//to export the markdown
 module.exports = generateMarkdown;
