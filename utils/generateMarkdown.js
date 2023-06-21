7a905c3c64d292f76f14ba272a115ba60d2be665
4FunkE <4FunkEcodes@gmail.com>
2023-06-20 20:33:39 -0400

    created generate readme function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,49 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Badges] (#badges)
+  - [Features](#features)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Credits
+  ${data.credits}
+
+  ## License
+  This application is covered under the ${data.license} license. [![License](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})
+
+  ## Badges
+  ${data.badges}
+
+  ## Features
+  ${data.features}
+
+  ## How to Contribute
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  For additional questions, contact me through my [GitHub profile](https://github.com/${data.githubUsername}) or via email at ${data.email}.
 `;
 }
 
