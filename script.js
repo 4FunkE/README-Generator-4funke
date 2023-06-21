c5f8f916b77f761a8c26b5e5bd03424d2e81b8db
4FunkE <4FunkEcodes@gmail.com>
2023-06-20 20:19:15 -0400

    created questions array following README

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ const inquirer = require("inquirer")//the inquiry files from JSON
 const generateMarkdown = require("./utils/generateMarkdown")//download markdown file
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the name of your project repo?",
+    "Please provide a short description explaining the what, why, and how of your project.",
+    "What are the steps required to install your project?",
+    "Please provide instructions and examples of the applications use.",
+    "List your collaborators.",
+    "Choose a liscense:",
+    "Any badges? If none type n/a.",
+    "Does you project have any features? If none type n/a.",
+    "Provide guidelines for how other developers can contribute. If none type n/a.",
+    "Enter test instructions:",
+    "Enter your GitHub username:",
+    "Enter your email address:"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
