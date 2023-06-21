321831106eae92e7ac00f36be819b7053efc6960
4FunkE <4FunkEcodes@gmail.com>
2023-06-20 20:09:22 -0400

    included packages needed to download in js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const fs = require("fs")//to read and write files
+const inquirer = require("inquirer")//the inquiry files from JSON
+const generateMarkdown = require("./utils/generateMarkdown")//download markdown file
 
 // TODO: Create an array of questions for user input
 const questions = [];
