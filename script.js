4ab55613cc7f981ed755d88d40344f6e0d64706a
4FunkE <4FunkEcodes@gmail.com>
2023-06-20 20:43:37 -0400

    created the actual prompt

--- script.js
+++ script.js
@@ -19,6 +19,72 @@ const questions = [
     "Enter your email address:"
 ];
 
+//what will prompt the user for the information to generate README
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the name of your project repo?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Please provide a short description explaining the what, why, and how of your project.',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'What are the steps required to install your project?',
+      name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please provide instructions and examples of the applications use.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'List your collaborators.',
+        name: 'credits',
+    },
+    {
+        type: 'list',
+        message: 'Choose a liscense:',
+        name: 'license',
+        choices: ['MIT', 'Apache-2.0', 'GPL-3.0'],
+    },
+    {
+        type: 'input',
+        message: 'Any badges? If none type n/a.',
+        name: 'badges',
+    },
+    {
+        type: 'input',
+        message: 'Does you project have any features? If none type n/a.',
+        name: 'features',
+    },
+    {
+        type: 'input',
+        message: 'Provide guidelines for how other developers can contribute. If none type n/a.',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Enter test instructions:',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username:',
+        name: 'githubUsername',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email address:',
+        name: 'email',
+    },
+  ])
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile("README.md", readmeContent, (err) => {
